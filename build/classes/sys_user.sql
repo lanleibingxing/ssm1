9e76abea45c3c364a442da1da7eb62e071635b2e
lanleibingxing <46591310+lanleibingxing2@users.noreply.github.com>
2019-07-01 14:38:01 +0800

    首次上传ssm

---
+++ build/classes/sys_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO `sys_user` VALUES (5, 'm0003', '小军', '123', '1');
+INSERT INTO `sys_user` VALUES (6, 'm0001', '小红', '123', '1');
+INSERT INTO `sys_user` VALUES (7, 'm0001', '小明', '123', '1');
+INSERT INTO `sys_user` VALUES (8, 'm0001', '小红', '123', '1');
